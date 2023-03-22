819dab6272e4d74a28e150354dde8afbe9fdf3ee
kchristin22 <christinakoutsou22@gmail.com>
2023-03-22 17:24:31 +0200

    Fix stack of statistics reporting

--- inc/FreeRTOSTasks/StatisticsReportingTask.hpp
+++ inc/FreeRTOSTasks/StatisticsReportingTask.hpp
@@ -9,7 +9,7 @@ public:
      */
     void execute();
 
-    const static inline uint16_t TaskStackDepth = 550;
+    const static inline uint16_t TaskStackDepth = 2000;
 
     StackType_t taskStack[TaskStackDepth];
 
